d979edd6953f38b1e4374d91eb26bcb4daaa618b
hugodnpm <hugosc@gmail.com>
2021-01-27 20:24:04 -0300

    consertando pagina repositories

--- pages/Repositorios.js
+++ pages/Repositorios.js
@@ -1,35 +1,39 @@
-import React from 'react'
-/*
+import React from "react";
 
 const Index = ({ repos, user }) => {
-    return (
-        <div className='container mx-auto'>
-            <h1 className='text-5xl'>MEUS REPOSITÓRIOS</h1>
-            <h2 className='font-bold text-3xl'>Meus Repositórios no GitHub</h2>
-            <p>Github stats: Public Repos: {user.public_repos} / Public_gists: {user.public_gists} / Followers: {user.followers} </p>
-            {repos.map(repo => {
-                return (
-                    <div key={repo.id} className='rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md'>
-                        <h3 className='font-bold'>{repo.full_name}</h3>
-                        <p>language: {repo.language} / Starts: {repo.stargazers_count} </p>
-                    </div>
-                )
-
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-5xl">MEUS REPOSITÓRIOS</h1>
+      <h2 className="font-bold text-3xl">Meus Repositórios no GitHub</h2>
+      <p>
+        Github stats: Public Repos: {user.public_repos} / Public_gists:{" "}
+        {user.public_gists} / Followers: {user.followers}{" "}
+      </p>
+      {repos.map((repo) => {
+        return (
+          <div
+            key={repo.id}
+            className="rounded bg-gray-200 mx-8 my-4 p-4 hover:shadow-md"
+          >
+            <h3 className="font-bold">{repo.full_name}</h3>
+            <p>
+              language: {repo.language} / Starts: {repo.stargazers_count}{" "}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export async function getServerSideProps(context) {
-    const request = await fetch(process.env.API_URL + '/api/getUser')
-    const { repos, user } = await request.json()
+  const request = await fetch(process.env.API_URL + "/api/getUser");
+  const { repos, user } = await request.json();
 
-
-    return {
-        props: {
-            repos,
-            user
-        }
-    }
+  return {
+    props: {
+      repos,
+      user,
+    },
+  };
 }
-export default Index
-*/
+export default Index;
