14de3fe336167c38e24c86166d09c83a29ec561a
hugodnpm <hugosc@gmail.com>
2021-05-11 16:28:24 -0300

    consertanto tailwid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-    theme:{
-        fontFamily: {
-            sans: ['Roboto']
-        },
-        extend:{
-            colors:{
-                bluePale: 'rgba(66, 153, 225, var(--bg-opacity))'
-            }
-        }
-    }
-}
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Roboto"],
+    },
+    extend: {
+      colors: {
+        bluePale: "rgba(66, 153, 225, var(--bg-opacity))",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
